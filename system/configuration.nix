bed70bf3f2a34d32b7c820720b7c5c628a48fa50
tom-tubeless <lutz.tomala@gmail.com>
2021-12-21 10:43:36 +0100

    configured nano

--- system/configuration.nix
+++ system/configuration.nix
@@ -185,11 +185,28 @@
       };
     };
     nano.nanorc = ''
-      unset backup
-      set nonewlines
-      set nowrap
-      set tabstospaces
+      set atblanks
+      set autoindent
+      set brackets ""')>]}"
+      set linenumbers
+      set magic
+      set matchbrackets "(<[{)>]}"
+      set mouse
+      set rawsequences
+      set smarthome
+      set softwrap
       set tabsize 4
+      set tabstospaces
+      set titlecolor bold,lightwhite,blue
+      set promptcolor lightwhite,lightblack
+      set statuscolor bold,lightwhite,green
+      set errorcolor bold,lightwhite,red
+      set selectedcolor lightwhite,magenta
+      set stripecolor ,yellow
+      set scrollercolor cyan
+      set numbercolor cyan
+      set keycolor cyan
+      set functioncolor green
     '';
   };
 
