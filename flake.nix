f4566e769501cbb16d03b60acc255f30d98b4553
tom-tubeless <lutz.tomala@gmail.com>
2021-12-21 08:42:11 +0100

    added flakes.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, nixpkgs-unstable, ... }: {
+    nixosConfigurations = {
+      testbox = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ({ config, pkgs, ... }:
+            let
+              overlay-unstable = final: prev: {
+                unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+              };
+            in
+            {
+              nixpkgs.overlays = [ overlay-unstable ];
+
+              imports =
+                [
+                  ./system/hardware-configuration.nix
+                  ./system/configuration.nix
+#                  ./users/dev_users.nix
+                ];
+            }
+          )
+        ];
+      };
+    };
+  };
+}
