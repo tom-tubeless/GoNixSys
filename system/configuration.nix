f59526ce736f144a58c35710ab1bd7e67b0d3b60
Lutz Tomala <7481954+tom-tubeless@users.noreply.github.com>
2021-12-24 11:50:52 +0000

    changed passwordFile location

--- system/configuration.nix
+++ system/configuration.nix
@@ -137,7 +137,7 @@
       extraGroups = [ "users" "wheel" "networkmanager" "docker"];
       # mkpasswd --method=SHA-512 --rounds=4096 'password' 'salt' and put in in the file
       # hashedPassword = "$6$ZaYKHBFurTaVvrjA$zhtaS9VAttn95Op3ny5GE2sbn2d29.660/yQnRbTyazVL3fXn7qUV//v21fhKeQR6fwtZI7300X8TBzJlDuIt/";
-      passwordFile = "./secrets/lgo-pw";
+      passwordFile = "/etc/lgo-pw";
       home = "/home/lgo";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK8E9qXTKVGPUVFynznaYEUwXCEyjNjE/CYmlciqKpmh tom1@xps-15" ];
